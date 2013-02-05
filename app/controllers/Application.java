c339938e6d145a90f8a1640d8dab9a7111f72c60
iijijii <lilysatoo@gmail.com>
2013-02-05 22:07:03 +0900

    Preparing the application

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,18 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-    return ok("Hello world.");
+      return redirect(routes.Application.tasks());
   }
-  
+    public static Result tasks() {
+        return TODO;
+    }
+    
+    public static Result newTask() {
+        return TODO;
+    }
+    
+    public static Result deleteTask(Long id) {
+        return TODO;
+    }
+    
 }
