cdbb75bc3bbc944d11d342894eb54c06a2a0bb06
iijijii <lilysatoo@gmail.com>
2013-02-19 09:51:27 +0900

    Handling the form submission

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,30 +1,33 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import models.Task;
 
-import views.html.*;
-
-import play.data.*;
-import models.*;
+import com.sun.j3d.utils.scenegraph.io.retained.Controller;
 
 public class Application extends Controller {
-   
-    static Form<Task> taskForm = Form.form(Task.class);
-    
-  public static Result index() {
-      return redirect(routes.Application.tasks());
-  }
-    public static Result tasks() {
-        return TODO;
-    }
-    
-    public static Result newTask() {
-        return TODO;
-    }
-    
-    public static Result deleteTask(Long id) {
-        return TODO;
-    }
-    
+
+	static Form<Task> taskForm = Form.form(Task.class);
+
+	public static Result index() {
+		return redirect(routes.Application.tasks());
+	}
+
+	public static Result tasks() {
+		return TODO;
+	}
+
+	public static Result newTask() {
+		Form<Task> filledForm = taskForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			return badRequest(views.html.index.render(Task.all(), filledForm));
+		} else {
+			Task.create(filledForm.get());
+			return redirect(routes.Application.tasks());
+		}
+	}
+
+	public static Result deleteTask(Long id) {
+		return TODO;
+	}
+
 }
