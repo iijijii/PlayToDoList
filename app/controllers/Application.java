fae44405b39ffd6d329f3888472cec029659ad0d
iijijii <lilysatoo@gmail.com>
2013-02-25 09:19:32 +0900

    Rendering the first page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,29 +1,30 @@
 package controllers;
 
-import models.Task;
+import play.*;
+import play.mvc.*;
 
-import com.sun.j3d.utils.scenegraph.io.retained.Controller;
+import views.html.*;
+
+import play.data.*;
+import models.*;
+
 
 public class Application extends Controller {
 
 	static Form<Task> taskForm = Form.form(Task.class);
 
 	public static Result index() {
-		return redirect(routes.Application.tasks());
-	}
+        return redirect(routes.Application.tasks());
+    }
 
 	public static Result tasks() {
-		return TODO;
+		return ok(
+       views.html.index.render(Task.all(), taskForm)
+                  );
 	}
 
 	public static Result newTask() {
-		Form<Task> filledForm = taskForm.bindFromRequest();
-		if (filledForm.hasErrors()) {
-			return badRequest(views.html.index.render(Task.all(), filledForm));
-		} else {
-			Task.create(filledForm.get());
-			return redirect(routes.Application.tasks());
-		}
+		return TODO;
 	}
 
 	public static Result deleteTask(Long id) {
