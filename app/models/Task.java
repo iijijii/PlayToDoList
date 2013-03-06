bdb5731d459e795224b7da94473b8301c443c160
iijijii <lilysatoo@gmail.com>
2013-03-06 18:48:00 +0900

    Persist the tasks in a database

--- app/models/Task.java
+++ app/models/Task.java
@@ -1,20 +1,31 @@
 package models;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import play.data.validation.Constraints.*;
+import play.db.ebean.*;
 
-public class Task {
-	public Long id;
+import javax.persistence.*;
+
+@Entity
+public class Task extends Model{
+	@Id
+    public Long id;
+    
 	@Required
 	public String label;
-
+    
+    public static Finder<Long,Task> find = new Finder(
+            Long.class, Task.class
+    );
+    
 	public static List<Task> all() {
-		return new ArrayList<Task>();
+		return find.all();
 	}
 
 	public static void create(Task task) {
-	}
+        task.save();
+    }
 
 	public static void delete(Long id) {
-	}
+        find.ref(id).delete();}
 }
